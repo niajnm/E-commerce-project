88826d221706aa0c7a49b5f8ad1053f317c45bd9
niajnm <niaj.nm@gmail.com>
2024-06-04 03:35:06 +0600

    1. resistration function

--- lib/app/module/user_auth/controller/auth_view_model.dart
+++ lib/app/module/user_auth/controller/auth_view_model.dart
@@ -1,27 +1,17 @@
 import 'dart:developer';
+
+import 'package:e_commerce/app/core/services/service_locator.dart';
 import 'package:e_commerce/app/data/local/preference/preference_manager.dart';
 import 'package:e_commerce/app/data/remote/remote-auth/api_model/LoginResponseModel.dart';
 import 'package:e_commerce/app/data/remote/remote-auth/api_model/login_post_param.dart';
+import 'package:e_commerce/app/data/remote/remote-auth/api_model/registration_post_param.dart';
+import 'package:e_commerce/app/data/remote/remote-auth/api_model/registration_res_model.dart';
 import 'package:e_commerce/app/data/repository/auth_repository/auth_repository.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:e_commerce/app/core/services/service_locator.dart';
-import 'package:e_commerce/app/data/remote/weather_remote/model/weather_params.dart';
-import 'package:e_commerce/app/data/remote/weather_remote/model/weather_response_model.dart';
-import 'package:e_commerce/app/data/repository/weather_repository/weather_repository.dart';
-import 'package:e_commerce/app/module/weather/ui_model/weather_ui_model.dart';
-import 'package:e_commerce/app/utils/location/location_service.dart';
-import 'package:geolocator/geolocator.dart';
 
 class UserAuthViewModel extends ChangeNotifier {
-  final WeatherRepository _weatherRepository =
-      serviceLocator<WeatherRepository>();
-
   final AuthRepository _authenticationRepository =
       serviceLocator<AuthRepository>();
-
-  String? cityName = 'Loading..!';
-
-  WeatherUIModel? weatherData;
 
   Future<LoginResponseModel> logInApiService(userName, userPassword) async {
     final userRepository = serviceLocator<PreferenceManager>();
@@ -48,19 +38,17 @@ class UserAuthViewModel extends ChangeNotifier {
     }
   }
 
-  Future<LoginResponseModel> userResistation(userName, userPassword) async {
-    final userRepository = serviceLocator<PreferenceManager>();
-
-    LoginPostParams param =
-        LoginPostParams(userName: userName, pass: userPassword);
+  Future<RegistrationResponseModel> userRegistration(
+      userName, userPassword) async {
+    RegistrationPostParam param = RegistrationPostParam(
+        username: 'username', email: 'n00@gmail.com', password: 'password');
     log('login param $userName $userPassword');
-    LoginResponseModel response =
+    RegistrationResponseModel response =
         await _authenticationRepository.userLogin(param);
 
     log('login param $userName $userPassword');
 
-    if (response.token != null) {
-      userRepository.setString("token", "$response.token");
+    if (response.code == 200) {
       return response;
     } else {
       // var error = LoginResponseModel(
