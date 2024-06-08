1872601e6d41ee28ab2f9df0c12bd49169a2517a
niajnm <niaj.nm@gmail.com>
2024-06-08 07:23:12 +0600

    1. service locator update

--- lib/app/core/services/service_locator.dart
+++ lib/app/core/services/service_locator.dart
@@ -2,14 +2,12 @@ import 'package:e_commerce/app/data/local/preference/preference_manager.dart';
 import 'package:e_commerce/app/data/local/preference/preference_manager_impl.dart';
 import 'package:e_commerce/app/data/remote/remote_auth/auth_remote_source.dart';
 import 'package:e_commerce/app/data/remote/remote_auth/auth_remote_source_impl.dart';
-import 'package:e_commerce/app/data/remote/weather_remote/weather_remote_source.dart';
-import 'package:e_commerce/app/data/remote/weather_remote/weather_remote_source_impl.dart';
+
 import 'package:e_commerce/app/data/repository/auth_repository/auth_repository.dart';
 import 'package:e_commerce/app/data/repository/auth_repository/auth_repository_impl.dart';
 import 'package:e_commerce/app/data/repository/product_repository/product_repository.dart';
 import 'package:e_commerce/app/data/repository/product_repository/product_repository_impl.dart';
-import 'package:e_commerce/app/data/repository/weather_repository/weather_repository.dart';
-import 'package:e_commerce/app/data/repository/weather_repository/weather_repository_impl.dart';
+
 import 'package:get_it/get_it.dart';
 
 final serviceLocator = GetIt.instance;
@@ -19,13 +17,7 @@ class ServiceLocator {
     serviceLocator.registerLazySingleton<PreferenceManager>(
         () => PreferenceManagerImpl());
 
-    // Register the WeatherRemoteDataSource in the data module
-    serviceLocator.registerLazySingleton<WeatherRemoteSource>(
-        () => WeatherRemoteSourceImpl());
-
-    serviceLocator.registerLazySingleton<WeatherRepository>(
-        () => WeatherRepositoryImpl());
-
+// initialization
     serviceLocator
         .registerLazySingleton<AuthRemoteSource>(() => AuthRemoteSourceImpl());
     serviceLocator.registerLazySingleton<AuthRepository>(
